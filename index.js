ebe3c990dec5cd6b9a0d30bad841e9a7f66ea9b1
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-30 19:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //
 $(document).ready(function(){
   //
+  function listenForClicksOnCells() {
   $(".cell").click(function() {
     console.log(this)
   })
+  }  
   //
          $("#square").fadeOut(2000);
     // $("#close-square").click(function(){
