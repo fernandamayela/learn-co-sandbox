383016468207fb66513f084cdb7f5be1c6fdddcc
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-30 18:49:29 +0000

    automatically backed up by learn

---
+++ other.js
@@ -0,0 +1,16 @@
+var firstNumber = 32
+var secondNumber = 9
+var thirdNumber = 14
+var fourthNumber = 33
+var fifthNumber = 48
+var powerBall = 5
+
+function logWinningNumbers(first, second, third, fourth, fifth, power) {
+  console.log("Winning numbers, "), firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, powerBall
+}
+
+logWinningNumbers(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, powerBall)
+
+var myArray = [31, 9, 14, 33, 48, 5]
+
+myArray.length
