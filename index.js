fe305a787deda572b25cbc82e4f1d08ec87e8f48
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-30 20:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 $(document).ready(function(){
   //
   var winArr = {
-    ["#0-0", "#1-0", "#2-0"], //
+    ["#0-0", "#1-0", "#2-0"], // top row
+    ["#0-1", "#1-1", "#2-1"] // middle row
+    ["#0-2", "#1-2", "#2-2"] // bottom row
+    ["#0-0", "#0-1", "#0-2"] // left column
+    ["#1-0", "#1-1", "#1-2"] // middle column
+    ["#2-0", "#2-1", "#2-2"] // right column
+    ["#0-0", "#1-1", "#2-2"] // black slash
+    ["#0-2", "#1-1", "2-0"] // forward slash
   }
   //
   function listenForClicksOnCells() {
