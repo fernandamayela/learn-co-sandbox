469d19830219405591014dae9e38549c69b12c99
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-30 19:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //
 $(document).ready(function(){
+  //
+  $(".cell").click(function() {
+    console.log(this)
+  })
+  //
          $("#square").fadeOut(2000);
     // $("#close-square").click(function(){
     //     $("#square").fadeOut(200);
